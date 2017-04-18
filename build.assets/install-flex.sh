48e07262d18fd53f5b60ea61e211277b156f0e45
Alex Peters <alpe@users.noreply.github.com>
2017-04-18 15:24:19 +0200

    Minor fixes for k8s env

--- build.assets/install-flex.sh
+++ build.assets/install-flex.sh
@@ -2,4 +2,4 @@
 
 mkdir -p /var/lib/spiffe/flex/spiffe.io~flex/creds
 cp -f /opt/spiffe/flex /var/lib/spiffe/flex/spiffe.io~flex
-cp -rf /var/lib/spiffe/creds/* /var/lib/spiffe/flex/spiffe.io~flex/
+cp -rf --dereference  /var/lib/spiffe/creds/* /var/lib/spiffe/flex/spiffe.io~flex/
